3adf5eea5fcc90d73373153e8106254a391128a4
Dmitriy Praslov <Mitrand.90@gmail.com>
2020-09-24 11:04:09 +0300

    Realization First-TZ 24.09

--- src/Practic/FirstTestExercise.java
+++ src/Practic/FirstTestExercise.java
@@ -3,59 +3,34 @@ package Practic;
 public class FirstTestExercise {
     public static void main(String[] args){
         Test1 firstTry = new Test1();
-        firstTry.test(3);
+        System.out.println(firstTry.test(6));
     }
 }
+
 class Test1 {
     public int test(int n) {
-        String[] mas = new String[n];
-        int i = 0;
+        int count = n;
         int result = 0;
-        int step = 0;
-        while (i<n){
-            if(i==0) {
-                mas[i] = "*";
-                result=mas[i].length();
-                System.out.println(mas[i]);
-            }
-            if (i>0) {
-                mas[i]=mas[i-1]+"*";
-                result = result+mas[i].length();
-                System.out.println(mas[i]);
-            }
-            if(n-result>=mas[i].length()+1){
-                System.out.println("like");
-            }
-            if (result>n){
-                System.out.println(step);
+        int temp = n;
+        for (int i=0;i<n;i++){
+            count=count-(i+1);
+            if (count>=0){
+                result++;
+                printStar(result);
+                temp = temp-result;
+            } else {
                 break;
             }
-            i++;
         }
-        return step;
+        printStar(temp);
+    return result;
+    }
+
+    public void printStar(int count){
+        for (int i=0;i<count;i++){
+            System.out.print("*");
+        }
+        System.out.println();
     }
 }
-//        int counter = 0;
-//        char[][] mas = new char [n][];
-//        char k = '*';
-//        for (int i=0; i<n; i++){
-//            mas[i] = new char [i+1];
-//            if (n-counter>=counter+1){
-//                result++;
-//            }
-//            for (int j=0; j<i+1; j++){
-//                if (counter<n) {
-//                    mas[i][j] = k;
-//                    System.out.print(mas[i][j]);
-//                    counter++;
-//                }
-//            }
-////            if (n<=counter){
-////                result++;
-////            }
-//            System.out.println();
-//        }
-//        System.out.println(result);
-//        return result;
-//    }
-//}
+
