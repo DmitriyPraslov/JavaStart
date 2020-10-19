571a7598b4f1ec4cd63e09a0c685ec56c5ad7f5e
Dmitriy Praslov <Mitrand.90@gmail.com>
2020-10-19 16:54:20 +0300

    Swap two int[] Array

---
+++ src/Practic/SwapArray.java
@@ -0,0 +1,23 @@
+package Practic;
+
+import java.util.Arrays;
+
+public class SwapArray {
+    public static void main(String[] args) {
+        int[] arr1 = {2,5,8,3,1,7};
+        int[] arr2 = {9,2,6,5,7};
+        swap(arr1, arr2);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+    }
+
+    public static void swap (int[] firstArray, int[] secondArray){
+        int stepCount = firstArray.length>=secondArray.length? secondArray.length : firstArray.length;
+        int temp;
+        for (int i=0;i<stepCount;i++){
+            temp = firstArray[i];
+            firstArray[i] = secondArray[i];
+            secondArray[i] = temp;
+        }
+    }
+}
