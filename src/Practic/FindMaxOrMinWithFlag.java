a6e304e06e82e22c3e713e04cbdc732ccb8351a0
Dmitriy Praslov <Mitrand.90@gmail.com>
2020-10-19 18:00:09 +0300

    Find Max Or Min With Flag in int[] Array

--- src/Practic/FindMaxOrMinWithFlag.java
+++ src/Practic/FindMaxOrMinWithFlag.java
@@ -10,7 +10,6 @@ public class FindMaxOrMinWithFlag {
         int[] newArray = {2,4,77,34,2,-1,-3,10};
         System.out.println(findMaxOrMin(newArray, flag));
     }
-
     public static int findMaxOrMin(int[] arr, int flag){
         int result = arr[0];
         for (int temp : arr){
