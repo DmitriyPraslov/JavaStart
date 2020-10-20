0d8f93011ca36eab4b9913b41842cb0b0feee7e3
niknemo2009 <nezdoliy.yura@gmail.com>
2020-10-20 05:53:31 +0300

    Update SwapArray.java
    +

--- src/Practic/SwapArray.java
+++ src/Practic/SwapArray.java
@@ -11,7 +11,7 @@ public class SwapArray {
         System.out.println(Arrays.toString(arr2));
     }
 
-    public static void swap (int[] firstArray, int[] secondArray){
+    public static void swap (int[] firstArray, int[] secondArray){//все добре
         int stepCount = firstArray.length>=secondArray.length? secondArray.length : firstArray.length;
         int temp;
         for (int i=0;i<stepCount;i++){
