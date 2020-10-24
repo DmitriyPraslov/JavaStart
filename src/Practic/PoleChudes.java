0cccc10cadf45d04cb40ab5e2050709ed76df638
Dmitriy Praslov <Mitrand.90@gmail.com>
2020-10-24 22:28:28 +0300

    E-olimp Exercise

---
+++ src/Practic/PoleChudes.java
@@ -0,0 +1,54 @@
+package Practic;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class PoleChudes {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter word:");
+        System.out.println(findCountStep(scan.nextLine()));
+
+    }
+
+
+    public static int findCountStep(String word){
+        int result = 0;
+        int iter = word.length();
+        String temp;
+        String[] wordArray = convertWordToStringArray(word);
+        for (int i=0;i<wordArray.length;i++) {
+            temp = wordArray[i];
+            if (temp!="*"){
+                for (int j=i;j<wordArray.length;j++) {
+                    if (temp==wordArray[j]){  // не работает (( - даже когда temp == wordArray[j]
+                        wordArray[j]="*";
+                        iter--;
+                    }
+                }
+                if (iter>=0){
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static String[] convertWordToStringArray(String word){
+        String[] result = new String[word.length()];
+        for (int i=0;i<word.length();i++){
+            result[i] = Character.toString(word.charAt(i));
+        }
+        return result;
+    }
+
+    public static int findCountStep1(String word){
+        Set<Character> charcollection = new HashSet<>();
+        for (int i=0;i<word.length();i++){
+            charcollection.add(word.charAt(i));
+        }
+        return charcollection.size();
+    }
+}
---
+++ src/Practic/SumProduct.java
@@ -0,0 +1,20 @@
+package Practic;
+
+import java.util.Scanner;
+
+public class SumProduct {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your number");
+        int a,b,c;
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+        calc(a,b,c);
+    }
+    public static void calc (int grn, int kop, int count){
+        int result = (grn*100+kop)*count;
+        String intToString = Integer.toString(result);
+        System.out.println(intToString.substring(0,intToString.length()-2)+" "+intToString.substring(intToString.length()-2,intToString.length()));
+    }
+}
