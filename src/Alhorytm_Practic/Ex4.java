78a549c688ac7c6d2b8ad0aafce5aeb8ce8a7514
Praslov Dmitriy <mitrand.90@gmail.com>
2021-03-18 23:09:02 +0200

    Algorytm 4 exercise

---
+++ src/Alhorytm_Practic/Ex4.java
@@ -0,0 +1,28 @@
+package Alhorytm_Practic;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        String[] arr = {"wrgkwo", "dq34fdsg", "24f", "kfjghq509"};
+        System.out.println(findMaxSumOfElement(arr));
+    }
+    public static String findMaxSumOfElement(String[] arr){
+        String result = "";
+        int maxSum = sumElement(arr[0]);
+        for (int i=0;i<arr.length;i++){
+            if (sumElement(arr[i])>=maxSum){
+                result = arr[i];
+                maxSum = sumElement(arr[i]);
+            }
+        }
+//        System.out.println(sumElement(result));
+        return result;
+    }
+
+    public static int sumElement(String str){
+        int result = 0;
+        for (int i=0;i<str.length();i++){
+            result += str.charAt(i);
+        }
+        return result;
+    }
+}
