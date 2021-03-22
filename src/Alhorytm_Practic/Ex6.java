5606c2d6c7e09447d590c3c29d47803a41c0bdcf
Praslov Dmitriy <mitrand.90@gmail.com>
2021-03-22 20:48:05 +0200

    MultipleDigit

---
+++ src/Alhorytm_Practic/Ex6.java
@@ -0,0 +1,27 @@
+package Alhorytm_Practic;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        MultipleDigit m1 = new MultipleDigit();
+        int[] arr = {2,4,8};
+        System.out.println(m1.findMin(arr));
+    }
+}
+class MultipleDigit{
+    int findMin (int[] arr){
+        int result = 0;
+        boolean findResultStatus = false;
+        while (!findResultStatus){
+            result++;
+            for (int temp : arr){
+                if (result%temp==0){
+                    findResultStatus = true;
+                } else {
+                    findResultStatus = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
